424a548539f0ec0d1d7517e14883a03723452f18
Claudbest1 <claudiusluv50@gmail.com>
2023-06-30 18:33:12 +0100

    changes on Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import { Hero } from '../data'
 const hero = () => {
   return (
 
-    <div className='md:px-[60px] sm:px-[20px] mt-[100px] md:mb-[100px] sm:mb-[40px] mt-[100px] md:mt-[200px]'>
+    <div className='md:px-[60px] px-[30px] mt-[100px] md:mb-[100px] sm:mb-[40px] mt-[100px] md:mt-[200px] '>
       <ul className='flex gap-10 overflow-hidden'>
       {
         Hero.map(({ img, category, date, title, paragraph, author, timeStamp }, index) => {
@@ -18,16 +18,16 @@ const hero = () => {
         <img src={img} alt="Ferris wheel" className='w-full sm:mb-5'/>
       </div>
       <div>
-        <div className='md:mb-10 lg:mb-2 xl:mb-5 sm:mb-2'>
-          <span className='mr-5 text-blue'>{category}</span>
+        <div className='md:mb-10 lg:mb-2 xl:mb-5 sm:mb-2 p-2'>
+          <span className='mr-5 text-blue font-sans1 font-medium text-[14px]'>{category}</span>
           {date}
         </div>
-        <h2 className='font-bold lg:text-[40px] xl:text-[40px] 2xl:text-[52px] xl:mb-5 font-sans md:mb-10 sm:mb-4 lg:mb-2 sm:text-[24px]'>{title}</h2>
-        <p className='lg:mb-2 xl:mb-5'>
+        <h2 className='font-bold lg:text-[40px] xl:text-[40px] 2xl:text-[52px] xl:mb-5 font-sans md:mb-10 sm:mb-4 lg:mb-2 text-[24px]'>{title}</h2>
+        <p className='lg:mb-2 xl:mb-5 font-sans1 mb-4'>
           {paragraph}
         </p>
-        <div>
-          <span className='mr-5 text-blue'>{author}</span>
+        <div className='text-[14px]'>
+          <span className='mr-5 text-blue font-sans1'>{author}</span>
           {timeStamp}
         </div>
       </div>
